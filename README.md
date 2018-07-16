6fbb109f34a4d6b464f9ebf4b2f63a73942c7607
varan <varan@VARAN-PC>
2018-07-17 02:25:46 +0300

    add script

--- README.md
+++ README.md
@@ -1,19 +1,44 @@
 # Prettify JSON
 
-[TODO. There will be project description]
+---
+The script reads the json file and writes the edited data to a json file
+---
 
-# Quickstart
-
-[TODO]
+### Quickstart
++ Script takes the path to the files as a parameters
++ Script checks the path, if there is no file - prints an error
++ Script prints an error if the file is not json
 
 Example of script launch on Linux, Python 3.5:
 
 ```#!bash
-
-$ python pprint_json.py <path to file>
-# TODO add output example
-
+$ python pprint_json.py <path to input file> <path to output file> 
 ```
+
+### Example output information from a file
+```bash
+{
+    "features": [
+        {
+            "geometry": {
+                "coordinates": [
+                    37.39703804817934,
+                    55.740999719549094
+                ],
+                "type": "Point"
+            },
+            "properties": {
+                "Attributes": {
+                    "Address": "улица Академика Павлова, дом 10",
+                    "AdmArea": "Западный административный округ",
+                    "ClarificationOfWorkingHours": null,
+                    "District": "район Кунцево",
+                    "IsNetObject": "да",
+                    "Name": "Ароматный Мир",
+                    "OperatingCompany": "Ароматный Мир",
+                    "PublicPhone": [
+```
+
 
 # Project Goals
 
--- pprint_json.py
+++ pprint_json.py
@@ -1,13 +1,49 @@
 import json
+import argparse
 
 
 def load_data(filepath):
-    pass
+    with open(filepath, "r", encoding="utf-8") as data_file:
+        data = json.load(data_file)
+    return data
 
 
-def pretty_print_json(data):
-    pass
+def pretty_output_json(data, filepath):
+    with open(filepath, encoding='utf-8', mode='w+') as output_file:
+        json.dump(
+            data,
+            output_file,
+            ensure_ascii=False,
+            sort_keys=True,
+            indent=4
+        )
 
 
-if __name__ == '__main__':
-    pass
+def get_parser_args():
+    parser = argparse.ArgumentParser(
+        description="Input and output files"
+    )
+    parser.add_argument(
+        "input_file",
+        help="Input path to the input file"
+    )
+    parser.add_argument(
+        "output_file",
+        help="Input path to the output file"
+    )
+    arguments = parser.parse_args()
+    return arguments
+
+
+if __name__ == "__main__":
+    arguments = get_parser_args()
+    if arguments.input_file is None or arguments.output_file is None:
+        exit("Error: You did not enter paths to the input and output files!")
+    try:
+        json_data = load_data(arguments.input_file)
+    except FileNotFoundError:
+        exit("Error: file or path '{0}' not found!\n".format(arguments.input_file))
+    except json.JSONDecodeError:
+        exit("Error: this is not json-file!")
+    pretty_output_json(json_data, arguments.output_file)
+
